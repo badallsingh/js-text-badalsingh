ee320447fd3dc584502b04dc563ddbf5f6892454
badallsingh <badalsingh931038@gmail.com>
2025-07-04 09:11:02 +0000

    conversion of datatypes

--- 01_basics/03_conversionoperation.js
+++ 01_basics/03_conversionoperation.js
@@ -1,10 +1,10 @@
 let score = "33"
  
-console.log (typeof score); console. log (typeof(score));
+// console.log (typeof score); console. log (typeof(score));
 
 let valueInNumber = Number (score)
 
-console.log (typeof valueInNumber);
+// console.log (typeof valueInNumber);
 
 
 // "33" => 33
@@ -17,7 +17,7 @@ let isLoggedIn = 1
 
 let booleanIsLoggedIn = Boolean (isLoggedIn)
 
-console. log(booleanIsLoggedIn);
+// console. log(booleanIsLoggedIn);
 
 // 1 → true; 0 = false
 // ""=> false
@@ -25,5 +25,37 @@ console. log(booleanIsLoggedIn);
 
 let someNumber = 33
 let stringNumber = String (someNumber)
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+
+// *****************Operations ******************
+
+let value = 3
+let negValue = -value
+// console. log (negValue);
+
+
+ // console.log (2+2);
+// console. log (2-2) ;
+// console. log (2*2);
+// console. log (2**3);
+// console.log(2/3);
+// console. log (2%3);
+
+let str1 = "hello"
+let str2 = " hitesh"
+let str3 = strl + str2
+// console. log(str3);
+
+//************types of solution *************************
+
+// console. log ("1" + 2);
+// console. log (1 + "2"); 
+// console.log("1" + 2 + 2);
+// console.log (1 + 2+ "2" );
+
+let gameCounter = 100
+++gameCounter; //postfix
+gameCounter++; // prefix 
+// console. log (gameCounter);
