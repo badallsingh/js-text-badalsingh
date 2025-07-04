71cd98448461cc44dcafe2b6d43c723130f1566f
badallsingh <badalsingh931038@gmail.com>
2025-07-04 07:47:36 +0000

    datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,17 @@
+const accountid =144553
+let accountemail ="badalsinghgmail/com"
+var accountpassword ="12345"
+accountcity = "jaipur"
+let accountstate 
+
+//  node 01_basics/01_variable.jsaccountid = 122 
+ 
+
+
+ accountemail ="badal.om"
+ accountpassword ="678910"
+accountcity = "haryana"
+console.log(accountid);
+
+  
+console.table([ accountid, accountemail, accountpassword, accountcity,accountstate])
---
+++ 01_basics/02_datatype.js
@@ -0,0 +1,24 @@
+ "use strict";// trest all js code as new version 
+
+// alert(3 + 3) //we are using nodejs, not broser
+
+let
+name = "badal"
+let age = 18
+let isLoggedIn = false
+// number = 2 to power 53
+// bigint
+// string →> "badal"
+// boolian -> true/fulse
+// null=> standalone value 
+// undifined => 
+// symbol=> unique
+
+// object 
+
+console.log(typeof "badal")
+console.log(typeof age)
+console.log(typeof null) // object
+console. log (typeof undefined); // unde console. log
+
+
