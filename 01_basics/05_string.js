1c20832d428924ae0f0ee0096c6f878bdb95dd5c
badallsingh <badalsingh931038@gmail.com>
2025-07-07 17:20:30 +0000

    strings topic fulll

---
+++ 01_basics/05_string.js
@@ -0,0 +1,34 @@
+const name = "badalsingh"
+const repoCount = 50
+// console.log (name + repoCount + " Value");
+
+
+
+console. log (`hello my name is ${name} and my repo count is ${repoCount}`);// it is newly used type of an output
+
+// another mathod of display string :-
+
+const gameName = new String( 'badal_singh_ity') //other type of string writing 
+// console. log (gameName[0]);  // to find the position of strings
+// console. log (gameName.__proto__); // use to find prototypes of an object
+
+// console. log (gameName.length); // to access the prototypes 
+// console. log (gameName. toUpperCase()) ; // methods  used in prototypes/string 
+
+// console. log (gameName.charAt (2));// methods  used in prototypes/string 
+//  console. log (gameName. indexOf ('t'));// methods  used in prototypes/string 
+
+const newString = gameName. substring (0, 4) // methods  used in prototypes/string 
+// console. log (newString);
+
+const anotherString = gameName.slice (-8, 4);
+// console. log (anotherString);
+const newStringOne = "     badalsingh.    "
+// console. log (newStringOne);
+// console. log (newStringOne.trim); // used to remove extra space
+
+const url = "https://hitesh.com/badal%20choudhary"// it basically webste name in over js
+// console.log (url. replace ('%20', '-'))  // to repace any thing 
+// console. log(url. includes ('badal')) // to find any thing 
+
+console.log(gameName.split('_'));
