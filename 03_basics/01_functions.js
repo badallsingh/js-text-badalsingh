1f16e5ce7a17f8fd9f76e5b910968411a724d9c4
badallsingh <badalsingh931038@gmail.com>
2025-07-14 07:32:17 +0000

    functions basic

---
+++ 03_basics/01_functions.js
@@ -0,0 +1,38 @@
+// ******************function*************************
+
+function myname() { ///dafination of func.
+console. log ("b") ;
+console.log("a");
+console.log("d");
+console. log("a");
+console.log("l");
+}
+
+myname ()  /// print a func.
+
+// additon
+
+// function addnumbers(number1,number2) {       ////numbers like(number1,number2) is parameters of func.
+//     console.log(number1 + number2);
+    
+// }
+
+// addnumbers(45,796)
+// addnumbers(45,"35")
+// addnumbers(45,null)   /// (45,null) they all are a arguments
+
+function addTwoNumbers (number1 , number2){  /// batter/smart type to 
+
+let result = number1 + number2;
+//  console.log ("Result: ", result);  /// find the value to result like numbers,boolean,string
+//  return result or return number1 + number2 
+
+}
+ 
+// addTwoNumbers(3,5)
+
+function loginUserMessage (username) {   /// type. to print vsrisble
+return  ` ${username} just logged in `
+}
+
+console.log(loginUserMessage("badalsingh"))
