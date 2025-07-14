4c0036fa65ab3dcf9a87a6f91b6c8a66e0c4333a
badallsingh <badalsingh931038@gmail.com>
2025-07-14 16:43:42 +0000

    full scope

---
+++ 03_basics/02_scopes.js
@@ -0,0 +1,50 @@
+// ***************** global scopes************************
+
+//var c = 300
+
+let a = 300
+if (true) {
+let a = 10
+const b = 20
+// console.log("INNER: ", a);
+}
+
+for
+(let i = 0; i < array.length; i++) {
+const element = arraylil;
+}
+
+// ******************* nested scopes **********************
+
+function one(){
+ const username = "hitesh"
+
+  function two() { 
+   const website = "youtube"
+    //  console.log(username);
+
+    }
+//   console.log(website);
+    //    two()
+}
+// one()
+
+if (true) {
+    const username ="badalsingh"
+    if (username === " badalsingh") {
+        const website = "youtube"
+        // console.log(username +website );
+        
+    }
+}
+
+function addone (num) {     //// same intersting functions
+return num + 1
+}
+addone (5)
+
+const addtwo = function (num) {    /// this is also a function but is stored in a (variable :-) variable stored avery thing in js
+    return num + 2;
+}
+
+addtwo(5)
