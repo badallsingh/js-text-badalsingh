81312accc77f22c3b39e31fdabc8333b36f98d01
badallsingh <badalsingh931038@gmail.com>
2025-07-15 15:54:28 +0000

    iffe complete

---
+++ 03_basics/04_iife.js
@@ -0,0 +1,13 @@
+// Immediately Invoked Function Expressions (IIFE)
+
+ (function chai(){      /// (function statment)() is called iffe 
+    console.log(`data bass connected`);
+ }) (); /// ()is used to execution
+
+ ( chaitwo = (name) => {     /// for arrow function
+    console.log(`databass connected two ${name}`); /// by useing variable
+    
+ }) ("badalsingh")
+
+ 
+
