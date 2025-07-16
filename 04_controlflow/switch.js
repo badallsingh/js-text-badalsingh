af22c78b5e8983e9307e1385cbf283f4ed68f7ae
badallsingh <badalsingh931038@gmail.com>
2025-07-16 07:54:35 +0000

    new folder

---
+++ 04_controlflow/switch.js
@@ -0,0 +1,34 @@
+// switch (key) {            ///// basic sintex of switch
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+const month = 3
+
+switch (month) {
+    case 1:
+        console.log("january");
+        
+        break;
+    case 2:
+        console.log("febuary");
+        
+        break;
+    case 3:
+        console.log("march");
+        
+        break;
+    case 4:
+        console.log("april");
+        
+        break;
+
+    default:
+        console.log("defoult case match ");
+        
+        break;
+}
