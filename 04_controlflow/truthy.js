1d2be4fc1cf8746570f5628c0d00bcea0d63c9db
badallsingh <badalsingh931038@gmail.com>
2025-07-16 17:15:05 +0000

    new file

---
+++ 04_controlflow/truthy.js
@@ -0,0 +1,21 @@
+// falsy values
+//  false, 0，-0, BigInt 0n，"", null, undefined, NaN accept those values all are truthy 
+
+//truthy values (same inportant)
+// "O",'false', " ",[], {},function(){}
+
+const useremail = []
+ 
+if
+(Object.keys(useremail).length === 0) {
+console.log ("array is empty");
+
+}
+
+const emptyobj = {}
+
+if
+(Object.keys(emptyobj).length === 0) {
+  console.log ("Object is empty");
+}
+
