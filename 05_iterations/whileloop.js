4e0ba159f57d8c6cb00e7636121d6ea1087e95d9
badallsingh <badalsingh931038@gmail.com>
2025-07-17 03:53:35 +0000

    coomplete

---
+++ 05_iterations/whileloop.js
@@ -0,0 +1,25 @@
+// ******************* while , do while loop *******************
+
+let index = 0  
+while (index<=10 ) {
+    console.log(`the value of index is : ${index}`);
+    
+}
+
+const myArray = ["batmen","superman","ironmen"]
+
+let arr = 0
+while (arr == myArray.length ) {
+    console.log(`the arr is ${myArray[arr]}`);
+    arr ++
+}
+
+// do while loop 
+
+let score =  1
+
+do {
+    console.log(`score is :${score}`);
+    score ++
+} while (score <= 10);
+
